f08498b7d88850bdcc4e35e84bafc5cf969fb121
hkrnnkw <4901108@gmail.com>
2022-03-23 12:44:17 +0900

    feat: selector of x times per day

--- src/components/CustomDrawer/Frequency/XTimesPerDay.tsx
+++ src/components/CustomDrawer/Frequency/XTimesPerDay.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react'
-import { Select } from '@mui/material'
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material'
 
 export type XTimesPerDayType = {
   x: number
@@ -11,9 +11,32 @@ type Props = {
 }
 
 const XTimesPerDay: FC<Props> = ({ frequency: { x } }) => {
-  console.log('XTimesPerDay')
+  const [xTime, setXTime] = useState(x.toString())
 
-  return <Select />
+  const handleChange = (event: SelectChangeEvent) => {
+    setXTime(event.target.value)
+  }
+
+  return (
+    <Select
+      labelId="select-x-times"
+      id="select-x-times"
+      value={xTime}
+      label="xTime"
+      onChange={handleChange}
+    >
+      <MenuItem value={1}>1</MenuItem>
+      <MenuItem value={2}>2</MenuItem>
+      <MenuItem value={3}>3</MenuItem>
+      <MenuItem value={4}>4</MenuItem>
+      <MenuItem value={5}>5</MenuItem>
+      <MenuItem value={6}>6</MenuItem>
+      <MenuItem value={7}>7</MenuItem>
+      <MenuItem value={8}>8</MenuItem>
+      <MenuItem value={9}>9</MenuItem>
+      <MenuItem value={10}>10</MenuItem>
+    </Select>
+  )
 }
 
 export default XTimesPerDay
