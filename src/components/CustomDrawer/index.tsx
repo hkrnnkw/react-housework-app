4342771d0630d7594ad8e41b69baa8cff9585ece
hkrnnkw <4901108@gmail.com>
2022-04-19 10:22:57 +0900

    feat: add puller

--- src/components/CustomDrawer/index.tsx
+++ src/components/CustomDrawer/index.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { FC } from 'react'
 import { css } from '@emotion/react'
-import { List, ListItem, SwipeableDrawer } from '@mui/material'
+import { Box, List, ListItem, styled, SwipeableDrawer } from '@mui/material'
+import { grey } from '@mui/material/colors'
 import { HouseworkDetail, HouseworkId } from '../../lib/type'
 import { State as UserState } from '../../contexts/user/constants'
 import Frequency from './Frequency'
@@ -9,6 +10,16 @@ import { useHouse } from '../../contexts/houses'
 import Point from './Point'
 import Member from './Member'
 import Title from './Title'
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 40,
+  height: 6,
+  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}))
 
 type Props = {
   houseworkId: HouseworkId
@@ -34,6 +45,7 @@ const CustomDrawer: FC<Props> = ({
       onClose={() => toggleDrawer(null)}
       onOpen={() => toggleDrawer(houseworkId)}
     >
+      <Puller />
       <List>
         <ListItem css={listItem}>
           <Title
