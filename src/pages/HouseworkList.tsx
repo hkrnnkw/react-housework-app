1f1f8e3579ac83c1bda2fa721a9b31d94c54fb95
hkrnnkw <4901108@gmail.com>
2022-04-27 01:04:54 +0900

    feat: add fab

--- src/pages/HouseworkList.tsx
+++ src/pages/HouseworkList.tsx
@@ -1,12 +1,16 @@
+/** @jsxImportSource @emotion/react */
 import { FC } from 'react'
 import { Link as RouterLink, Outlet } from 'react-router-dom'
 import {
+  Fab,
   Link,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
 } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import { css } from '@emotion/react'
 import { useHouse } from '../contexts/houses'
 
 export const Index: FC = () => {
@@ -15,30 +19,45 @@ export const Index: FC = () => {
   if (!currentHouse || !houses) return null
   const { housework } = houses[currentHouse.id]
 
+  const handleAddTask = () => {
+    console.log('add task')
+  }
+
   return (
-    <List>
-      {Object.entries(housework).map(([categoryId, { taskDetails }]) =>
-        Object.entries(taskDetails).map(([taskId, detail]) => {
-          const id = `${categoryId}-${taskId}`
-          return (
-            <Link key={id} component={RouterLink} to={id} state={{ id }}>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemText
-                    id={id}
-                    primary={detail.title}
-                    secondary={detail.point}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </Link>
-          )
-        })
-      )}
-    </List>
+    <>
+      <List>
+        {Object.entries(housework).map(([categoryId, { taskDetails }]) =>
+          Object.entries(taskDetails).map(([taskId, detail]) => {
+            const id = `${categoryId}-${taskId}`
+            return (
+              <Link key={id} component={RouterLink} to={id} state={{ id }}>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText
+                      id={id}
+                      primary={detail.title}
+                      secondary={detail.point}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            )
+          })
+        )}
+      </List>
+      <Fab color="primary" aria-label="add" css={fab} onClick={handleAddTask}>
+        <AddIcon />
+      </Fab>
+    </>
   )
 }
 
 const HouseworkList: FC = () => <Outlet />
 
 export default HouseworkList
+
+const fab = css`
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+`
