864394c9caaa2017176265cb89ddf20a179b6b6d
hkrnnkw <4901108@gmail.com>
2022-06-13 12:01:41 +0900

    refactor: set midnight as current date default

--- src/lib/states/currentDate.ts
+++ src/lib/states/currentDate.ts
@@ -5,5 +5,10 @@ import { DATE_FORMAT } from '../constant'
 // eslint-disable-next-line import/prefer-default-export
 export const stateCurrentDate = atom<string>({
   key: 'stateCurrentDate',
-  default: dayjs().format(DATE_FORMAT),
+  default: dayjs()
+    .hour(0)
+    .minute(0)
+    .second(0)
+    .millisecond(0)
+    .format(DATE_FORMAT),
 })
